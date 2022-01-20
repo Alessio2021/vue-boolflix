e896fc1379d335bfe3587a651dd2882238713c3d
Alessio2021 <alessiobut1911@gmail.com>
2022-01-20 12:49:07 +0100

    fix query

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 .<template>
   <header>
-      <input @keyup.enter="getAxios" type="text" placeholder="Search" v-model="inputSearch">
-      <button @click="getAxios">Cerca</button>
+      <input @keyup.enter="getFilm" type="text" placeholder="Search" v-model="inputSearch">
+      <button @click="getFilm">Cerca</button>
   </header>
 </template>
 
@@ -12,21 +12,24 @@ export default {
     name: 'Header',
     data() {
         return {
-           inputSearch: '',
-           movies: null,
-           inputFixed: null,
-           
+            inputSearch: '',
+            movies: null,
+            inputFixed: null,
+            api_key: '401d2b8a5e51bdf6caf8b6e192d59d74',
+            apiStatic: 'https://api.themoviedb.org/3/search/',
+            language: 'en-US',       
         }
     },
     methods: {
-        getAxios() {
+        getFilm() {
+            const endpoint = 'movie';
+            const parameters = {
+                api_key: this.api_key,
+                language: this.language,
+                query: this.inputSearch,
+            };
             this.inputFixed = this.inputSearch.replace(/ /g,"+");
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=401d2b8a5e51bdf6caf8b6e192d59d74',
-            {
-                params: {
-                    query:this.inputFixed
-                }
-            })
+            axios.get(`${this.apiStatic}${endpoint}`, { params: parameters })
             .then((result) => {
                 this.movies = result.data.results
                 this.$emit('doSearch', this.movies)
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,10 +3,10 @@
       <ul>
           <li v-for="(movie, index) in searchMovieMain" :key="index">
               <img src="" alt="">
-              <h2>{{movie.title}}</h2>
-              <h2>{{ movie.original_title }}</h2>
-              <h2><i :class="(movie.original_language == 'en') ? 'flag flag-us' : `flag flag-${ movie.original_language }`"></i></h2>
-              <h2>{{ movie.vote_average }}</h2>
+              <h2>Titolo: {{movie.title}}</h2>
+              <h4>Titolo originale {{ movie.original_title }}</h4>
+              <h5>Lingua <i :class="(movie.original_language == 'en') ? 'flag flag-us' : `flag flag-${ movie.original_language }`"></i></h5>
+              <h5>Voto: {{ movie.vote_average }}</h5>
           </li>
       </ul>
   </main>
@@ -18,7 +18,7 @@ export default {
     props: ['searchMovieMain'],
     data() {
         return {
-            apiStatic: 'https://api.themoviedb.org/3/search/movie?api_key=401d2b8a5e51bdf6caf8b6e192d59d74&query='
+            
         }
     },
 }
